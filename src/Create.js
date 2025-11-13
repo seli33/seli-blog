d14cb84d9d045a2cb608601382f2d2af3a46902b
seli <selinamaharjan39@gmail.com>
2025-11-13 20:19:26 +0545

    changes

--- src/Create.js
+++ src/Create.js
@@ -3,6 +3,18 @@ const Create  = () => {
   return(
     <div className="create">
         <h2>Create a new blog</h2>
+        <form>
+          <label>Blog Title</label>
+          <input type="text" required/>
+          <label>Blog body:</label>
+          <textarea required></textarea>
+          <label>Blog author:</label>
+          <select>
+            <option value="mario">mario</option>
+            <option value="selina">selina</option>
+          </select>
+          <button>Add Blog</button>
+        </form>
     </div>
 
   );
