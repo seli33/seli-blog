c5f3eb27de18bea6f6b396b942037f647f473eae
seli <selinamaharjan39@gmail.com>
2025-11-13 09:52:42 +0545

    changes

---
+++ src/BlogList.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const BlogList = ({ blogs, title, handleDelete}) => {
+  return (
+    <div className="blog-list">
+      <h2>{title}</h2>
+      {blogs.map((blog) => (
+        <div className="blog-preview" key={blog.id}>
+          <h3>{blog.title}</h3>
+          <p>Written by {blog.author}</p>
+          <p>{blog.body}</p>
+          <button onClick ={() =>handleDelete(blog.id)}>Delete Blog </button>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default BlogList;
--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {useState } from 'react';
+import {useState,useEffect } from 'react';
 import BlogList from './BlogList';
 
 // A simple stateless functional component
@@ -37,11 +37,22 @@ const Home = () => {
   }
   ]);
  
-  
+  const [name, setName]= useState('mario');
+
+  const handleDelete =(id)=>{
+    const newBlogs=blogs.filter(blog=>blog.id !==  id )
+    setBlogs(newBlogs)
+  }
+
+  useEffect(()=>{
+    console.log("this is use effect ");
+    console.log(name);
+  },[name]);
+
   return (
     <div className="home">
-     <BlogList blogs={blogs} title="All Blogs" />
-     <BlogList blogs={blogs.filter((blog)=> blog.author==='mario')} title="Mario's Blogs" />
+     <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
+     <button onClick ={()=>setName('luigi')}>change name</button>
            
      
     </div>
